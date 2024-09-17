b86aef1412b4798d774c4e7769f825eb4e6ce39a
naimur.naiyimu <naimur.naiyimu@gmail.com>
2024-09-17 17:04:06 +0600

    add Contract Model

--- contract/models.py
+++ contract/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Contact(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    email = models.EmailField()
+    phone_number = models.CharField(max_length=15, validators=[validate_phone_number])
+    address = models.TextField(blank=True, null=True)
+
+    def __str__(self):
+        return f'{self.first_name} {self.last_name} - {self.phone_number}'
--- contract/views.py
+++ contract/views.py
@@ -1,3 +1,5 @@
 from django.shortcuts import render
 
 # Create your views here.
+def contract_list(request):
+    pass
